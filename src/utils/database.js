474fb1f37e036eefc21e87040b5887170d2a3f72
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-09 15:20:09 -0500

    Updated routes in src/routes/router.js

---
+++ src/utils/database.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSE,
+    process.env.PGPASSWORD,
+    {
+        host: process.env.PGHOST,
+        dialect: 'postres'
+    }
+);
+
+
+module.exports = sequelize;
