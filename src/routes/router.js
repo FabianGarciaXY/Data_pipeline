5125a7ce2f35c68f93dd5e94acbc59bc19203b31
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-09 22:40:16 -0500

    Corrected callback functions in routers

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 
 
 router
-	.get('/', controller.app() )
-	.get('/vehicles', controller.getAllVehicles() )
+	.get('/', controller.app)
+	.get('/vehicles', controller.getAllVehicles)
 	.get('/vehicles/:id', (req, res) => {
 		res.json({
 			message: `La ubicación del vehículo con ID ${req.params.id} es : x`
@@ -14,12 +14,13 @@ router
 		res.json({
 			message: 'Hay X alcaldias disponibles'
 		});
-		router.get('/alcaldias/:id', (req, res) => {
-			res.json({
-				message: `Hay X metrobuces disponibles para la alcaldía ${req.params.id}`
-			});
-		});
+
 	});
+router.get('/alcaldias/:id', (req, res) => {
+	res.json({
+		message: `Hay X metrobuces disponibles para la alcaldía ${req.params.id}`
+	});
+});
 
 
 module.exports = router;
