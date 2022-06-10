d4859ab426f1a0d1343b3d03690b7dfbd2ff3987
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-09 23:14:46 -0500

    Changed all callback methods of router to controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,7 +2,8 @@ const Metrobuses = require('../models/model');
 
 let counter = 0;
 
-exports.app = (req, res) => {
+// Ruta raiz
+const app = (req, res) => {
 	try {
 		return res.status(200).json({ 'message': `This server has been visited ${counter++} times` });
 	} catch (err) {
@@ -10,11 +11,53 @@ exports.app = (req, res) => {
 	}
 };
 
-exports.getAllVehicles = async (req, res) => {
+// Obtener las unidades disponibles
+const getAvailableVehicles = async (req, res) => {
 	try {
 		const allVehicles = await Metrobuses.findAll();
 		return res.status(200).json(allVehicles);
 	} catch (err) {
 		return res.status(500).json(err);
 	}
+};
+
+// Consultar la ubicación de una unidad dado su ID
+const getVehicleLocationById = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const vehicle = await Metrobuses.findOne(); // Correct here		
+		return res.status(200).json(vehicle);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+};
+
+// Obtener una lista de alcaldías disponibles
+exports.getAvailableTowns = async (req, res) => {
+	try {
+		const allTowns = await Metrobuses.findAll();
+		return res.status(200).json(allTowns);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+};
+
+// Obtener la lista de unidades que se encuentren dentro de una alcaldía
+const getVehiclesByTown = async (req, res) => {
+	try {
+		const id = req.params.name;
+		const vehicle = await Metrobuses.findMany(); // Correct here		
+		return res.status(200).json(vehicle);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+};
+
+
+module.exports = {
+	app: app,
+	getAvailableVehicles: getAvailableVehicles,
+	getVehicleLocationById: getVehicleLocationById,
+	getAvailableTowns: getAvailableTowns,
+	getVehiclesByTown: getVehiclesByTown
 };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,26 +1,13 @@
-const controller = require('./../controllers/controller');
+const app = require('./../controllers/controller');
 const router = require('express').Router();
 
 
 router
 	.get('/', controller.app)
-	.get('/vehicles', controller.getAllVehicles)
-	.get('/vehicles/:id', (req, res) => {
-		res.json({
-			message: `La ubicación del vehículo con ID ${req.params.id} es : x`
-		});
-	})
-	.get('/alcaldias', (req, res) => {
-		res.json({
-			message: 'Hay X alcaldias disponibles'
-		});
-
-	});
-router.get('/alcaldias/:id', (req, res) => {
-	res.json({
-		message: `Hay X metrobuces disponibles para la alcaldía ${req.params.id}`
-	});
-});
+	.get('/metrobuses', controller.getAvailableVehicles)
+	.get('/metrobuses/:id', controller.getVehicleLocationById)
+	.get('/alcaldias', controller.getAvailableTowns)
+	.get('/alcaldias/:name', controller.getVehiclesByTown);
 
 
 module.exports = router;
