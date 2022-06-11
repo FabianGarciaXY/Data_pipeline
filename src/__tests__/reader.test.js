e837a811588e70b739fcaef09d9ecae71a7d2504
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-11 02:14:38 -0500

    Created and passed 2 unit tests to validate Metrobuses API with axios

--- src/__tests__/reader.test.js
+++ src/__tests__/reader.test.js
@@ -1,15 +1,19 @@
-const data = require('../utils/reader');
+const axios = require('axios').default;
+
+
+const url = 'https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e&limit=1000';
 
 
 describe('Suite de tests para validar la informacion de metrobuces de CDMX', () => {
 
-	test('Test 1: validando que la informacion sea recibida exista', () => {
-		expect(data).not.toBeUndefined();
+	test('Test 1: validando que la que el endpoint sea contenga los datos requeridos', async () => {
+		const response = await axios.get(url)
+		expect(response).not.toBeUndefined();
 	});
 
-	test('Test 2: validando la cantidad de registros obtenidos', () => {
-
-		const recordsQuantity = data.result.records.length;
-		expect(recordsQuantity).toBe(207);
+	test('Test 2: validando la cantidad de registros obtenidos', async () => {
+		const response = await axios.get(url)
+		const records = response.data.result.records.length;
+		expect(records).toBe(207);
 	});
 });
