78bf9a4042d807d5816b87d49d20ff2caf086cfc
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-11 02:13:42 -0500

    Corrected fetchData as asychronous function

--- src/utils/reader.js
+++ src/utils/reader.js
@@ -3,8 +3,8 @@ const axios = require('axios').default;
 
 const url = 'https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e&limit=1000';
 
-const getData = async () => {
-	const data = await axios.get(url);
-	return data;
-
-};
+(async function getData() {
+	const response = await axios.get(url);
+	const data = response.data.result;
+	console.log(data)
+})();
