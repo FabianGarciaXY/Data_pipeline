83b72ae542f1d859767033c73e7ca2f8801e93e7
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-17 17:03:14 -0500

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ const sequelize = require('./utils/database');
 const { getData, geocoderConfig } = require('./utils/seed');
 
 
-
 /* Here the server is initialized by importing the necessary libraries and modules: express, sequelize and routes.  */
 
 
-// The server port is defined, and express is executed.
-const port = process.env.PORT;
+// The Express application is executed and the routes imported from the router.js module are used.
 const app = express();
 app.use(routes);
 
 
 // A self-executing asynchronous function is declared and executed to initialize the services.
-(async () => {
+(async function main() {
 	try {
 		// The sequelize sync method is executed to perform the database migration.
+		// The getData() function initializes the fetching and saving of data in the database.
 		await sequelize.sync({force: true});
-		await getData(process.env.VEHICLESAPI, geocoderConfig);
+		await getData(process.env.VEHICLESAPI, geocoderConfig); 
 
-		app.listen(port, () => console.log('Server running on port', port));
+		app.listen(process.env.PORT, () => console.log('Server running on port', 4000));
 	} catch (err) {
 		console.log(err);
 	}
