10c65313cc2b2a9c74142c9b03cc08e11dba1f81
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-17 01:15:47 -0500

    Updatedn call for getData function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./routes/router');
-const getData = require('./utils/reader');
 const sequelize = require('./utils/database');
+const { getData, geocoderConfig } = require('./utils/seed');
+
 
 
 /* Here the server is initialized by importing the necessary libraries and modules: express, sequelize and routes.  */
@@ -18,7 +19,7 @@ app.use(routes);
 	try {
 		// The sequelize sync method is executed to perform the database migration.
 		await sequelize.sync({force: true});
-		getData();
+		await getData(process.env.VEHICLESAPI, geocoderConfig);
 
 		app.listen(port, () => console.log('Server running on port', port));
 	} catch (err) {
