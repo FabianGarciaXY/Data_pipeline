ff636b702accefb99e70a81a8b42b0a24dd58454
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-17 01:16:52 -0500

    Created seed.js and updated to reader.js scripts to fill database

--- src/utils/reader.js
+++ src/utils/reader.js
@@ -1,53 +1,87 @@
-const axios = require('axios').default;
 const Vehicle = require('../models/vehicles.model');
-const gecoder = require('geocoder');
+const axios = require('axios').default;
+const geocoder = require('geocoder');
 
 
-require('dotenv').config();
+/* This module contains all the functions to handle vehicle data collection and address conversion. */
 
 
-const getData = async () => {
-
-	const url = 'https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e&limit=4';
-	const response = await axios.get(url);
-	const data = response['data']['result']['records'];
-
-	for (let vehicle of data) {
-
-		const latitude = vehicle['position_latitude'];
-		const longitude = vehicle['position_longitude'];
-
-		await gecoder.reverseGeocode(latitude, longitude, async (err, data) => {
-
-			const fullAdress = data['results'][0]['formatted_address'];
-			const addressComponents = fullAdress.split(', ');
-			const delegation = addressComponents[addressComponents.length - 4].toLowerCase();
-
-			await Vehicle.create({
-				vehicle_id: vehicle['vehicle_id'],
-				vehicle_label: vehicle['vehicle_label'],
-				current_status: vehicle['vehicle_current_status'],
-				latitude: vehicle['position_latitude'],
-				longitude: vehicle['position_longitude'],
-				position_speed: vehicle['position_speed'],
-				position_odometer: vehicle['position_odometer'],
-				trip_schedule_relationship: vehicle['trip_schedule_relationship'],
-				trip_id: vehicle['trip_id'],
-				trip_start_date: vehicle['trip_start_date'],
-				trip_route_id: vehicle['trip_route_id'],
-				address: fullAdress,
-				delegation: delegation
-			});
-		},
-		{
-			key: process.env.GOOGLEMAPSKEY,
-			result_type: 'route',
-			language: 'es',
-		}
-		);
-	}
+// @Description: Function to get data from Metrobuses API using Axios library.
+// @param { url } the string url from the API.
+// @returns [ array] an array of objects containing the vehicles data.
+const getVehiclesData = async (url) => {
+	let response = await axios.get(url);
+	return response['data']['result']['records'];
 };
 
 
-// Exporting function
-module.exports = getData;
+// @Description: Function to get vehicles latitude and longitude.
+// @param [ data ] an object containing the data fetched from Metrubuses API through getVehiclesData Function.
+// @returns [ array ] an array of latitude and longitude coordinates.
+const getCoordinates = (data) => {
+	return data.map(vehicle => coords = { lat: vehicle['position_latitude'], long: vehicle['position_longitude'] });
+};
+
+
+// @Description: This Function saves the data fetched with getVehiclesData Function into metrobuses table of Metrobuses_db.
+// @param [ vehiclesData ] an array of vehicles object obtained with getVehiclesData Function.
+const saveVehiclesData = (vehiclesData) => {
+	
+	vehiclesData.forEach(async (vehicle) => {
+		await Vehicle.create({					// Data is saved in the db using the 'create' method.
+			vehicle_id: vehicle['vehicle_id'],
+			vehicle_label: vehicle['vehicle_label'],
+			current_status: vehicle['vehicle_current_status'],
+			latitude: vehicle['position_latitude'],
+			longitude: vehicle['position_longitude'],
+			position_speed: vehicle['position_speed'],
+			position_odometer: vehicle['position_odometer'],
+			trip_schedule_relationship: vehicle['trip_schedule_relationship'],
+			trip_id: vehicle['trip_id'],
+			trip_start_date: vehicle['trip_start_date'],
+			trip_route_id: vehicle['trip_route_id']
+		});
+	});
+};
+
+
+// @Description: Function to get and save the address and specific delegation of each vehicle using Geocoder library.
+// @param [ coords ] an array of objects containg coordinates of each vehicle.
+// @param { gecoderConfig } an object with the API Key and type of result to use Geocoder library.
+const saveVehiclesDelegations = (coords, geocoderConfig) => {
+
+	// Each element of the list is traversed and the geocoder is called to obtain the address.
+	coords.forEach((coord) => {
+		// A asynchrunous callback is used to handle the response of the geocoder.
+		geocoder.reverseGeocode(coord.lat, coord.long, async (err, data) => {
+			if (data) {
+				let address = data['results'][0]['formatted_address'];
+				let addressComponents = address.split(', ');
+				let delegation = addressComponents[addressComponents.length - 4].toLowerCase();
+
+				// Using the Vehicle model, the metrobuses table is updated with its corresponding delegations.
+				await Vehicle.update({
+					address: address,
+					delegation: delegation
+				}, 
+				{
+					where: { latitude: coord.lat }
+				});
+			} else if (err) {
+				throw new Error(err)
+			}
+			},
+			geocoderConfig  // Geocoder configuration
+		);
+	});
+};
+
+
+
+// Exporting function to use in seed.js module.
+module.exports = {
+	getVehiclesData,
+	getCoordinates,
+	saveVehiclesData,
+	saveVehiclesDelegations
+};
---
+++ src/utils/seed.js
@@ -0,0 +1,34 @@
+const { getVehiclesData, getCoordinates, saveVehiclesData, saveVehiclesDelegations } = require('./reader');
+require('dotenv').config();
+
+/*
+ * This module is used to fill the database with the metrobus API data,
+ * as well as its location using the Google geocoder API by using the midelwares of the reader.js file.
+*/
+
+
+// Configuration for geocoder from Google maps API that's used in saveVehiclesDelegations Function.
+const geocoderConfig = {
+	key: process.env.GOOGLEMAPSKEY,
+	result_type: 'route',
+	language: 'es'
+};
+
+
+// Main function
+const getData = async (url, geocoderConfig) => {
+
+	const vehiclesData = await getVehiclesData(url);
+	saveVehiclesData(vehiclesData);
+
+	const coordinates = await getCoordinates(vehiclesData);
+	saveVehiclesDelegations(coordinates, geocoderConfig);
+}
+
+
+// Exporting function
+module.exports = {
+	vehiclesApiUrl,
+	getData,
+	geocoderConfig
+};
