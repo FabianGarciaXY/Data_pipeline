abda7019d8bac12f41f513f6362061fa7311b6e8
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-17 17:50:14 -0500

    Updated comments to reader.js and database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,8 +2,7 @@ const Sequelize = require('sequelize');
 
 /* This module is for configuration and connection to postgres metrobuses_db database using Sequelize. */
 
-
-// The connection to the database is established using environment variables from docker-compose.yml.
+// The connection to the database is established using environment variables from docker-compose.yml
 const sequelize = new Sequelize(
 	process.env.PGDATABASE,
 	process.env.PGUSER,
--- src/utils/reader.js
+++ src/utils/reader.js
@@ -3,11 +3,14 @@ const axios = require('axios').default;
 const geocoder = require('geocoder');
 
 
-/* This module contains all the functions to handle vehicle data collection and address conversion. */
+/* 
+ * This module contains all the functions to handle vehicle data collection 
+ * and address conversion that will be used in seed.js module. 
+*/
 
 
-// @Description: Function to get data from Metrobuses API using Axios library.
-// @param { url } the string url from the API.
+// @Description: Asynchronous function to fetch data from Metrobuses API using Axios library.
+// @param { url } the string url.
 // @returns [ array] an array of objects containing the vehicles data.
 const getVehiclesData = async (url) => {
 	let response = await axios.get(url);
@@ -15,20 +18,20 @@ const getVehiclesData = async (url) => {
 };
 
 
-// @Description: Function to get vehicles latitude and longitude.
-// @param [ data ] an object containing the data fetched from Metrubuses API through getVehiclesData Function.
+// @Description: Function to get vehicles latitude and longitude coordinates.
+// @param [ data ] an array of objects containing the data obtained from the Metrubuses API using the getVehiclesData function.
 // @returns [ array ] an array of latitude and longitude coordinates.
 const getCoordinates = (data) => {
 	return data.map(vehicle => coordinates = { lat: vehicle['position_latitude'], long: vehicle['position_longitude'] });
 };
 
 
-// @Description: This Function saves the data fetched with getVehiclesData Function into metrobuses table of Metrobuses_db.
-// @param [ vehiclesData ] an array of vehicles object obtained with getVehiclesData Function.
+// @Description: This function saves the vehicles data in the metrobuses table of Metrobuses_db database.
+// @param [ vehiclesData ] an array of vehicles object obtained with getVehiclesData function.
 const saveVehiclesData = (vehiclesData) => {
 	
 	vehiclesData.forEach(async (vehicle) => {
-		await Vehicle.create({					// Data is saved in the db using the 'create' method.
+		await Vehicle.create({		// Data is saved in the db using the 'create' method of Vehicle Model.
 			vehicle_id: vehicle['vehicle_id'],
 			vehicle_label: vehicle['vehicle_label'],
 			current_status: vehicle['vehicle_current_status'],
@@ -47,19 +50,21 @@ const saveVehiclesData = (vehiclesData) => {
 
 // @Description: Function to get and save the address and specific delegation of each vehicle using Geocoder library.
 // @param [ coords ] an array of objects containg coordinates of each vehicle.
-// @param { gecoderConfig } an object with the API Key and type of result to use Geocoder library.
+// @param { gecoderConfig } an object with the settings (API Key and type of result) to use Geocoder library.
 const saveVehiclesDelegations = (coords, geocoderConfig) => {
 
-	// Each element of the list is traversed and the geocoder is called to obtain the address.
+	// Each element of the list is iterated and the geocoder is called to obtain the address.
 	coords.forEach((coord) => {
-		// A asynchrunous callback is used to handle the response of the geocoder.
+
+		// The reverseGocoder method accepts the two coordinates.
+		// And an asynchrunous callback function is used to handle the response of the geocoder.
 		geocoder.reverseGeocode(coord.lat, coord.long, async (err, data) => {
 			if (data) {
 				let address = data['results'][0]['formatted_address'];
 				let addressComponents = address.split(', ');
 				let delegation = addressComponents[addressComponents.length - 4].toLowerCase();
 
-				// Using the Vehicle model, the metrobuses table is updated with its corresponding delegations.
+				// Using the Vehicle model, the metrobuses table is updated with its corresponding addres and delegations.
 				await Vehicle.update({
 					address: address,
 					delegation: delegation
@@ -71,14 +76,15 @@ const saveVehiclesDelegations = (coords, geocoderConfig) => {
 				throw new Error(err);
 			}
 		},
-		geocoderConfig  // Geocoder configuration
+		// Geocoder configuration(API key)
+		geocoderConfig  
 		);
 	});
 };
 
 
 
-// Exporting function to use in seed.js module.
+// Exporting functions to use in seed.js module.
 module.exports = {
 	getVehiclesData,
 	getCoordinates,
